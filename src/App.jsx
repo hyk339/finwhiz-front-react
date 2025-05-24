803a6c60d7d95896c47f28a5c93d0f05a747db22
hyk <hyk339@gmail.com>
2025-05-24 22:35:02 +0900

    라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
 import './App.css'
+import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Notfound from './pages/Notfound'
 
+
+
+// 1. "/" : 뉴스를 조회하는 Home 페이지
 function App() {
- 
-
   return (
-    <>
-      <h1>Hello React!</h1>
-    </>
+   <Routes>
+    <Route path ="/" element={<Home/>} />
+    <Route path ="*" element={<Notfound/>} />
+   </Routes>
   )
 }
 
