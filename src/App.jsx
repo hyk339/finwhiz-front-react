8f352f38442db3c6010590811848513d7259f86f
hyk <hyk339@gmail.com>
2025-05-27 00:10:10 +0900

    Header, Button 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
 import Notfound from './pages/Notfound'
-
+import Button from './components/Button'
+import Header from './components/Header'
 
 
 // 1. "/" : 뉴스를 조회하는 Home 페이지
 function App() {
   return (
-   <Routes>
-    <Route path ="/" element={<Home/>} />
-    <Route path ="*" element={<Notfound/>} />
-   </Routes>
+    <>
+      <Header title={"제목입니다."} leftChild={"메뉴"} rightChild={"마이페이지"}></Header>
+      <Button text={"123"} type={"gray"}/>
+
+      <Routes>
+        <Route path ="/" element={<Home/>} />
+        <Route path ="*" element={<Notfound/>} />
+      </Routes>
+   </>
   )
 }
 
